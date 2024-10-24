9db5ebf6d812e22c63912ef5e22ffa6644813408
CiprianRad <89615038+CiprianRad@users.noreply.github.com>
2024-10-24 22:49:15 +0300

    Add files via upload

---
+++ Product_of_proper_divisors.py
@@ -0,0 +1,57 @@
+def get_valid_input():
+    while True:
+        try:
+            n = int(input("Enter an integer: "))
+            if n >= 0:
+                return n
+            else:
+                return abs(n)
+        except ValueError:
+            print("Invalid input. Please enter a valid integer: ")
+
+
+def get_list_of_proper_divisors(n):
+    divisor_list = []
+    for i in range(2, n // 2 + 1):
+        if n % i == 0:
+            divisor_list.append(i)
+            divisor_list.append(-i)
+    return divisor_list
+
+
+def test_list():
+    assert get_list_of_proper_divisors(6) == [2, -2, 3, -3]
+    assert get_list_of_proper_divisors(0) == []
+    assert get_list_of_proper_divisors(10) == [2, -2, 5, -5] 
+    assert get_list_of_proper_divisors(5) == []
+
+
+def multiply_numbers_in_list(list_of_proper_divisors):
+    product_of_divisors = 1
+    for divisor in list_of_proper_divisors:
+        product_of_divisors *= divisor
+    if list_of_proper_divisors == []:
+        return None
+    else:
+        return product_of_divisors
+
+
+def test_multiplication():
+    assert multiply_numbers_in_list([2, -2, 3, -3]) == 36
+    assert multiply_numbers_in_list([0, 1, 3]) == 0
+    assert multiply_numbers_in_list([10, 5, 2]) == 100
+    assert multiply_numbers_in_list([0]) == 0
+    assert multiply_numbers_in_list([-3, 10]) == 30
+
+
+def main():
+    
+    
+    n = get_valid_input()
+    list_of_divisors = get_list_of_proper_divisors(n)
+    product_of_divisors = multiply_numbers_in_list(list_of_divisors)
+    print("The proper divisors of the number", n, "are: ", list_of_divisors, "\nAnd their product is: ", product_of_divisors)
+    test_list()
+
+
+main()
