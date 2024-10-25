23b5441d0bd71781b42e178b9befc5b9231bfe8c
CiprianRad <89615038+CiprianRad@users.noreply.github.com>
2024-10-25 12:50:34 +0300

    Product_of_proper_divisors.py

--- Product_of_proper_divisors.py
+++ Product_of_proper_divisors.py
@@ -11,26 +11,26 @@ def get_valid_input():
 
 
 def get_list_of_proper_divisors(n):
-    divisor_list = []
+    divisor_list = []  # We declare a void list 
     for i in range(2, n // 2 + 1):
-        if n % i == 0:
-            divisor_list.append(i)
-            divisor_list.append(-i)
+        if n % i == 0:  # We check for proper divisors in the smallest range possible
+            divisor_list.append(i)  # We add them as elements in the list
+            divisor_list.append(-i)  # If a positive number is the divisor of a number than so is it's opposite
     return divisor_list
 
 
 def test_list():
     assert get_list_of_proper_divisors(6) == [2, -2, 3, -3]
     assert get_list_of_proper_divisors(0) == []
-    assert get_list_of_proper_divisors(10) == [2, -2, 5, -5] 
+    assert get_list_of_proper_divisors(10) == [2, -2, 5, -5]
     assert get_list_of_proper_divisors(5) == []
 
 
 def multiply_numbers_in_list(list_of_proper_divisors):
-    product_of_divisors = 1
+    product_of_divisors = 1  # We initiate the product of divisors with 1 since 1 is the neutral element
     for divisor in list_of_proper_divisors:
-        product_of_divisors *= divisor
-    if list_of_proper_divisors == []:
+        product_of_divisors *= divisor  # We multiply each divisor in the list together
+    if list_of_proper_divisors == []:  # In the case the number has no divisors the list will remain empty
         return None
     else:
         return product_of_divisors
@@ -45,13 +45,12 @@ def test_multiplication():
 
 
 def main():
-    
-    
     n = get_valid_input()
     list_of_divisors = get_list_of_proper_divisors(n)
     product_of_divisors = multiply_numbers_in_list(list_of_divisors)
     print("The proper divisors of the number", n, "are: ", list_of_divisors, "\nAnd their product is: ", product_of_divisors)
     test_list()
+    test_multiplication()
 
 
 main()
